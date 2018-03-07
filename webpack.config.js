e59de31abd7384943e0697e713d3ba5ac5ba5583
Jonathan Beliën <jbe@geo6.be>
2018-03-07 13:14:32 +0100

    Update Webpack 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,27 @@
-const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
-  entry: {
-    "batch-geocoder": "./resources/javascript/main.js"
-  },
-  output: {
-    filename: "[name].min.js",
-    path: path.resolve(__dirname, "public/js")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          {loader: "style-loader"},
-          {loader: "css-loader"}
+    mode: 'development',
+    entry: {
+        "batch-geocoder": "./resources/javascript/main.js"
+    },
+    output: {
+        filename: "[name].min.js",
+        path: path.resolve(__dirname, "public/js")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    },
+                    {
+                        loader: "css-loader"
+                    }
+                ]
+            }
         ]
-      }
-    ]
-  },
-  plugins: [
-    //new webpack.optimize.UglifyJsPlugin()
-  ],
-  resolve: {
-    alias: {
-      '@fortawesome/fontawesome-free-solid$': '@fortawesome/fontawesome-free-solid/shakable.es.js',
-      '@fortawesome/fontawesome-pro-regular$': '@fortawesome/fontawesome-pro-regular/shakable.es.js'
     }
-  }
 };
