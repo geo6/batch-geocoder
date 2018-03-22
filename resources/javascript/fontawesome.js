2f0a448ecd29ce6a1a45fd4c737b925a9402ad26
Jonathan Beliën <jbe@geo6.be>
2018-03-22 16:31:07 +0100

    Update fontawesome.js

--- resources/javascript/fontawesome.js
+++ resources/javascript/fontawesome.js
@@ -1,45 +1,40 @@
 import fontawesome from '@fortawesome/fontawesome';
 
-import {
-    faAngleDoubleRight,
-    faArchive,
-    faArrowCircleRight,
-    faArrowRight,
-    faCog,
-    faMapMarkerAlt,
-    faSync,
-    faUpload,
-    faUser
-} from '@fortawesome/fontawesome-free-solid';
-fontawesome.library.add(
-    faAngleDoubleRight,
-    faArchive,
-    faArrowCircleRight,
-    faArrowRight,
-    faCog,
-    faMapMarkerAlt,
-    faSync,
-    faUpload,
-    faUser
-);
+import faAngleDoubleRight from '@fortawesome/fontawesome-free-solid/faAngleDoubleRight';
+import faArchive from '@fortawesome/fontawesome-free-solid/faArchive';
+import faArrowCircleRight from '@fortawesome/fontawesome-free-solid/faArrowCircleRight';
+import faArrowRight from '@fortawesome/fontawesome-free-solid/faArrowRight';
+import faCog from '@fortawesome/fontawesome-free-solid/faCog';
+import faMapMarkerAlt from '@fortawesome/fontawesome-free-solid/faMapMarkerAlt';
+import faSync from '@fortawesome/fontawesome-free-solid/faSync';
+import faUpload from '@fortawesome/fontawesome-free-solid/faUpload';
+import faUser from '@fortawesome/fontawesome-free-solid/faUser';
 
-import {
-    faCheckCircle,
-    faEraser,
-    faLanguage,
-    faListAlt,
-    faMap,
-    faRocket,
-    faTable,
-    faTimesCircle
-} from '@fortawesome/fontawesome-pro-regular';
+import faCheckCircle from '@fortawesome/fontawesome-pro-regular/faCheckCircle';
+import faEraser from '@fortawesome/fontawesome-pro-regular/faEraser';
+import faLanguage from '@fortawesome/fontawesome-pro-regular/faLanguage';
+import faListAlt from '@fortawesome/fontawesome-pro-regular/faListAlt';
+import faMap from '@fortawesome/fontawesome-pro-regular/faMap';
+import faRocket from '@fortawesome/fontawesome-pro-regular/faRocket';
+import faTable from '@fortawesome/fontawesome-pro-regular/faTable';
+import faTimesCircle from '@fortawesome/fontawesome-pro-regular/faTimesCircle';
+
 fontawesome.library.add(
+    faAngleDoubleRight,
+    faArchive,
+    faArrowCircleRight,
+    faArrowRight,
     faCheckCircle,
+    faCog,
     faEraser,
     faLanguage,
     faListAlt,
     faMap,
+    faMapMarkerAlt,
     faRocket,
+    faSync,
     faTable,
-    faTimesCircle
+    faTimesCircle,
+    faUpload,
+    faUser
 );
